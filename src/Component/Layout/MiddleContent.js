23f9aa02fb176d3bf36c2d3197899f61b3a9e157
Kanika <88278472+kanikaacem@users.noreply.github.com>
2023-12-28 15:40:13 +0530

    Adding testimonial

--- src/Component/Layout/MiddleContent.js
+++ src/Component/Layout/MiddleContent.js
@@ -1,12 +1,13 @@
 import CustomizeCarousel from "../CustomizeCarousel";
 import Section2 from "../Section2";
 import Section3 from "../Section3";
-
+import Testimonial from "../Testimonial";
 function MiddleContent(){
     return (<>
     <CustomizeCarousel></CustomizeCarousel>
     <Section2></Section2>
     <Section3></Section3>
+    <Testimonial/>
     </>)
 }
 export default MiddleContent;
